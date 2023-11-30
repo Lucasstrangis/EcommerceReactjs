0c54f328369e56381aaa579780f148caa4e52d61
Lucas Strangis <lucasstrangis1@gmail.com>
2023-11-30 15:23:14 -0300

    estilos en los detail y list

--- src/components/ItemDetailConteiner/ItemDetailContainer.jsx
+++ src/components/ItemDetailConteiner/ItemDetailContainer.jsx
@@ -1,44 +1,46 @@
-import { useEffect, useState } from "react"
-import ItemDetail from '../ItemDetail/ItemDetail'
-import { useParams } from 'react-router-dom'
-import { getProductById } from "../../firebase/firestore/products"
+// ItemDetailContainer.jsx
+
+import { useEffect, useState } from "react";
+import ItemDetail from '../ItemDetail/ItemDetail';
+import { useParams } from 'react-router-dom';
+import { getProductById } from "../../firebase/firestore/products";
+import classes from './ItemDetailContainer.module.css';
 
 const ItemDetailContainer = () => {
-    const [product, setProduct] = useState(null)
-    const [loading, setLoading] = useState(true)
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const { itemId } = useParams()
+  const { itemId } = useParams();
 
-    useEffect(() => {
-        setLoading(true)
+  useEffect(() => {
+    setLoading(true);
 
-    
-        getProductById(itemId)
-            .then(response => {
-                setProduct(response)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-            .finally(() => {
-                setLoading(false)
-            })
-    }, [itemId])
+    getProductById(itemId)
+      .then(response => {
+        setProduct(response);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [itemId]);
 
-    if(loading) {
-        return <h1>Cargando Producto</h1>
-    }
+  if (loading) {
+    return <h1>Cargando Producto</h1>;
+  }
 
-    if(!product) {
-        return <h1>El producto no existe</h1>
-    }
+  if (!product) {
+    return <h1>El producto no existe</h1>;
+  }
 
-    return (
-        <main style={{ background: 'pink'}}>
-            <h1>Detalle del producto</h1>
-            <ItemDetail {...product}/>
-        </main>
-    )
-}
+  return (
+    <main className={classes.container}>
+      <h1>Detalle del producto</h1>
+      <ItemDetail {...product} />
+    </main>
+  );
+};
 
-export default ItemDetailContainer
+export default ItemDetailContainer;
